bddce39b37bb95452e5ca605974a21fe03ab7e63
nourharidy <nourharidy@yahoo.com>
2018-01-16 01:31:31 +0200

    Finished --all argument

--- decentube.js
+++ decentube.js
@@ -5,6 +5,7 @@ const WebTorrent = require("webtorrent-hybrid");
 const colors = require("colors");
 
 var config = {
+  "block":4678985, //Block number of the contract deployment
   "provider":"wss://mainnet.infura.io/ws",
   "address": "0x85be36FA32D11BA6070F60A0119F1Fc5b0d25D1D",
   "abi":[{"constant":false,"inputs":[{"name":"id","type":"uint256"},{"name":"title","type":"string"},{"name":"description","type":"string"}],"name":"updateVideo","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"names","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"removeVideo","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"magnet","type":"string"},{"name":"title","type":"string"},{"name":"description","type":"string"}],"name":"create","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"creators","outputs":[{"name":"addr","type":"address"},{"name":"name","type":"string"},{"name":"avatar","type":"string"},{"name":"bio","type":"string"},{"name":"coinhive","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"},{"name":"avatar","type":"string"},{"name":"bio","type":"string"},{"name":"coinhive","type":"string"}],"name":"profile","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"videos","outputs":[{"name":"magnet","type":"string"},{"name":"title","type":"string"},{"name":"description","type":"string"},{"name":"date","type":"uint256"},{"name":"tips","type":"uint256"},{"name":"creator","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"tip","outputs":[{"name":"","type":"bool"}],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Tip","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"magnet","type":"string"},{"indexed":false,"name":"creator","type":"address"},{"indexed":false,"name":"title","type":"string"},{"indexed":false,"name":"description","type":"string"},{"indexed":false,"name":"date","type":"uint256"}],"name":"Upload","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"addr","type":"address"},{"indexed":false,"name":"name","type":"string"},{"indexed":false,"name":"avatar","type":"string"},{"indexed":false,"name":"bio","type":"string"},{"indexed":false,"name":"coinhive","type":"string"}],"name":"Registration","type":"event"}]
@@ -16,6 +17,8 @@ var seed = function(data){
   var Decentube = new web3.eth.Contract(config.abi, config.address);
   var client = new WebTorrent();
 
+  console.log("Decentube node started".blue)
+
   //Seed all existing videos
   fs.readdir("./videos", (err, files) => {
     files.forEach(file => {
@@ -25,13 +28,27 @@ var seed = function(data){
   })
 
   if(data.id){ //If looking for a specific video id
-    //Seed video
+    Decentube.events.Upload({
+      fromBlock:config.block //Block number of the contract deployment
+    }, function(error, event){
+        console.log("hi")
+        if(event.returnValues[0] == data.id && client.get(event.returnValues.magnet) === null){
+          console.log(("Found video: "+event.returnValues.title + " (https://decentube.com/#/v/" + event.returnValues.id + ")").blue);
+          client.add(event.returnValues.magnet, {path:"./videos"}, function(torrent){
+            console.log(("STARTED downloading "+ event.returnValues.title + " (https://decentube.com/#/v/" + event.returnValues.id + ")").green)
+            torrent.on("done", function(){
+              console.log(("FINISHED downloading "+ event.returnValues.title + " (https://decentube.com/#/v/" + event.returnValues.id + ")").green)
+            })
+          })
+        }
+    })
   }else if(data.user){ //If looking for a specific user's videos
+  console.log("hi")
     Decentube.methods.names(web3.utils.soliditySha3(data.user)).call(function(error, namesResult){
       if(namesResult != "0x0000000000000000000000000000000000000000"){
         console.log(("User " + data.user+ " was found! Seeding his videos.").green);
         Decentube.events.Upload({
-          fromBlock:4678985 //Block number of the contract deployment
+          fromBlock:config.block
         }, function(error, event){
             if(event.returnValues.creator === namesResult && client.get(event.returnValues.magnet) === null){
               console.log(("Found video: "+event.returnValues.title + " (https://decentube.com/#/v/" + event.returnValues.id + ")").blue);
@@ -48,7 +65,20 @@ var seed = function(data){
       }
     })
   }else{ //If looking for all videos
-    console.log("Seeding all videos".green)
+    console.log("Seeding all videos".green);
+    Decentube.events.Upload({
+      fromBlock:config.block //Block number of the contract deployment
+    }, function(error, event){
+        if(client.get(event.returnValues.magnet) === null){
+          console.log(("Found video: "+event.returnValues.title + " (https://decentube.com/#/v/" + event.returnValues.id + ")").blue);
+          client.add(event.returnValues.magnet, {path:"./videos"}, function(torrent){
+            console.log(("STARTED downloading "+ event.returnValues.title + " (https://decentube.com/#/v/" + event.returnValues.id + ")").green)
+            torrent.on("done", function(){
+              console.log(("FINISHED downloading "+ event.returnValues.title + " (https://decentube.com/#/v/" + event.returnValues.id + ")").green)
+            })
+          })
+        }
+    })
   }
 }
 
